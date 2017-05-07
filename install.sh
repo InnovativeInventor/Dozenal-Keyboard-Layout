fb107ef76e7320c47b3ca6ba4ef835430e68dc60
Inventor <maxfan8@gmail.com>
2017-05-06 22:19:50 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo Installing duodecimal keyboard layout . . .
+sudo curl -O --location '/Library/Keyboard Layouts/' https://github.com/InnovativeInventor/Dozenal-Keyboard-Layout/raw/master/Files/Duodecimal.icns 
+sudo curl -O --location '/Library/Keyboard Layouts/' https://github.com/InnovativeInventor/Dozenal-Keyboard-Layout/raw/master/Files/Duodecimal.keylayout
+echo Your keyboard layout has been modified!
+echo
+echo To complete installation, go to System Preferences > Keyboard > Input Sources and add Duodecimal
+echo To use duodecimal symbols, press option-tilde for X, option-1 for E, and option-2 for 10
+echo Note: The tilde key is the key above tab and below esc
